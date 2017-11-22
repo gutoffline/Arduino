fe16d7f75cddb843936644d9ce057df288a29d1d
Guto Xavier <gutoffline@gmail.com>
2017-11-22 20:07:52 -0200

    Switch

---
+++ Aula5/SwitchAcendendoApagando/SwitchAcendendoApagando.ino
@@ -0,0 +1,39 @@
+#define LED_VERMELHO 13
+#define LED_AZUL 12
+#define LED_VERDE 11
+#define UM_SEGUNDO 1000
+
+void setup() {
+  pinMode(LED_VERMELHO,OUTPUT);
+  pinMode(LED_AZUL,OUTPUT);
+  pinMode(LED_VERDE,OUTPUT);
+  Serial.begin(9600); //configura comunicação serial com 9600 bps
+}
+ 
+void loop() {
+  if(Serial.available()){
+    switch(Serial.read()){
+      case 'A':
+        Mudar(LED_VERMELHO);
+        break;
+      case 'B':
+        Mudar(LED_AZUL);
+        break;
+      case 'C':
+        Mudar(LED_VERDE);
+        break;
+    }
+  }
+}
+
+void Mudar(int LED){
+  digitalWrite(LED, !digitalRead(LED));
+  if(digitalRead(LED)){
+    Serial.println("Aceso");
+  }else{
+    Serial.println("Apagado");
+  }
+}
+  
+  
+  
